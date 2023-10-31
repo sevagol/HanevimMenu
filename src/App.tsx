0db7204dfb713a24838d33887c8bba18193c8eb6
Lev Tomatny <mastergefest@gmail.com>
2023-10-31 23:33:30 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-
 import MenuItem from './MenuItem';
-import { Container, Grid, CssBaseline } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 const App = () => {
   const menuItems = [
-    { title: 'Coffee', price: 10.99, imgUrl: 'path/to/your/coffee/image.jpg' },
-    { title: 'Pancake', price: 7.99, imgUrl: 'path/to/your/pancake/image.jpg' },
+    { title: 'Coffee', price: 10.99, imgUrl: 'path/to/coffee/image.jpg'},
+    { title: 'Pancake', price: 7.99, imgUrl: 'path/to/pancake/image.jpg' },
     // ... другие позиции меню
   ];
 
   return (
-    <>
-      <CssBaseline />
-      <Container>
-        <Grid container spacing={3} alignItems="stretch">
-          {menuItems.map((item, index) => (
-            <Grid item xs={12} sm={3} key={index}>
-              <MenuItem {...item} />
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-    </>
+    <Container>
+      <Grid container spacing={3}>
+        {menuItems.map((item, index) => (
+          <Grid item xs={4} key={index}>
+            <MenuItem {...item} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
