d445470186b3d7c7b58218f141d6a849d47b6139
Lev Tomatny <mastergefest@gmail.com>
2023-11-01 06:59:47 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
+import { useState, useEffect } from 'react';
 import MenuItem from './MenuItem';
 import Cup from './assets/cofffee-cup.svg';
 import Pancake from './assets/pancakes.svg';
 import Tea from './assets/tea.svg';
 import './App.css';
-import { useState } from 'react';
 import WebApp from '@twa-dev/sdk';
 
 
 const App = () => {
+  const [addedItemsCount, setAddedItemsCount] = useState(0);
+
   const menuItems = [
-    { title: 'Coffee', price: 10.99, imgUrl: Cup},
+    { title: 'Coffee', price: 10.99, imgUrl: Cup },
     { title: 'Pancake', price: 7.99, imgUrl: Pancake },
-    { title: 'Tea', price: 3.99, imgUrl: Tea},
+    { title: 'Tea', price: 3.99, imgUrl: Tea },
   ];
 
-  const [isItemAdded, setIsItemAdded] = useState(false);
-
   const handleAddChange = (isAdded: boolean) => {
-    if (isAdded && !isItemAdded) {
-      setIsItemAdded(true);
-      const mainbutton = WebApp.MainButton;
-      mainbutton.setText('VIEW ORDER')
+    setAddedItemsCount((prevCount) => (isAdded ? prevCount + 1 : prevCount - 1));
+  };
+
+  useEffect(() => {
+    const mainbutton = WebApp.MainButton;
+
+    if (addedItemsCount > 0) {
+      mainbutton.setText("VIEW ORDER")
       mainbutton.show();
-    } else if (!isAdded && isItemAdded) {
-      setIsItemAdded(false);
-      const mainbutton = WebApp.MainButton;
+    } else {
       mainbutton.hide();
     }
-  };
+  }, [addedItemsCount]);
 
   return (
     <div className="menu-container">
