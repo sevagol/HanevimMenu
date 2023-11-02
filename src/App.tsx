78cdb9c7acf2e88f138c282f5715a4f23d4685f6
Lev Tomatny <mastergefest@gmail.com>
2023-11-02 10:48:53 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import MenuItem from './MenuItem';
+import OrderList from './OrderList'; // Предположим, что у вас есть такой компонент для отображения заказов
 import Cup from './assets/cofffee-cup.svg';
 import Pancake from './assets/pancakes.svg';
 import Tea from './assets/tea.svg';
 import './App.css';
 import WebApp from '@twa-dev/sdk';
 
-
 const App = () => {
-  WebApp.setBackgroundColor("#000000")
+  WebApp.setBackgroundColor("#000000");
   const [addedItemsCount, setAddedItemsCount] = useState(0);
+  const navigate = useNavigate();
 
   const menuItems = [
     { title: 'Coffee', price: 10.99, imgUrl: Cup },
@@ -25,19 +27,29 @@ const App = () => {
     const mainbutton = WebApp.MainButton;
 
     if (addedItemsCount > 0) {
-      mainbutton.setText("VIEW ORDER")
+      mainbutton.setText("VIEW ORDER");
       mainbutton.show();
+      mainbutton.onClick(() => {
+        navigate('/orders');
+      });
     } else {
       mainbutton.hide();
     }
   }, [addedItemsCount]);
 
   return (
-    <div className="menu-container">
-      {menuItems.map((item, index) => (
-        <MenuItem {...item} key={index} onAddChange={handleAddChange} />
-      ))}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={
+          <div className="menu-container">
+            {menuItems.map((item, index) => (
+              <MenuItem {...item} key={index} onAddChange={handleAddChange} />
+            ))}
+          </div>
+        } />
+        <Route path="/orders" element={<div></div>} />
+      </Routes>
+    </Router>
   );
 };
 
