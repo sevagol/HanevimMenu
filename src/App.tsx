86a3caa01d5353ed64186412986bd718ff8aa159
Lev Tomatny <mastergefest@gmail.com>
2023-11-02 18:27:01 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ const MainButtonLogic: React.FC<{ addedItemsCount: number }> = ({ addedItemsCoun
             mainbutton.show();
             mainbutton.onClick(() => {
                 navigate("/orders");
+                mainbutton.setText("ORDER NOW");
             });
         } else {
             mainbutton.hide();
